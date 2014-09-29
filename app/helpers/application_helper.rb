892c017b5dfd5d1180bcd00cfe4d45bbc0b36c4b
Sam Waller <Sam@sams-air.attlocal.net>
2014-09-29 12:25:51 -0400

    asset pipeline

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+    def user_login_links
+    if user_signed_in?
+      content_tag(:span, current_user.full_name, style: "color:red;") +
+        (link_to "Edit", edit_user_registration_path) +
+        (link_to "Log Out", destroy_user_session_path, method: :delete)
+    else
+      link_to "Log In", new_user_session_path
+    end
+  end
+end
