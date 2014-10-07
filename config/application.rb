220845655866c256924d60ec0135d1292cdeb44c
slwaller <hockeywalt@yahoo.com>
2014-10-07 15:08:25 -0400

    Woops wrong spot.

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,5 @@ module PizzaOne
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
-    config.assets.compile = true
   end
 end
